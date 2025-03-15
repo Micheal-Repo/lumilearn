26f8a49068c4ee55c3dff67d075a36ced0725cd1
Micheal-Repo <michealfullstack@gmail.com>
2025-03-15 23:00:06 +0100

    header

--- src/components/custom/clerkProviderWithTheme.tsx
+++ src/components/custom/clerkProviderWithTheme.tsx
@@ -1,20 +1,28 @@
 "use client";
 
 import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from '@clerk/themes'
+import { dark } from "@clerk/themes";
 import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
 
-export function ClerkProviderWithTheme({ children }: { children: React.ReactNode }) {
+export function ClerkProviderWithTheme({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const { theme } = useTheme();
-  const clerkTheme:any = theme === "dark" ? dark : null
- 
-  return (
-    <ClerkProvider
-      appearance={{
-         baseTheme: clerkTheme,
-      }}
-    >
-      {children}
-    </ClerkProvider>
-  );
+
+  if (theme === "dark") {
+    return (
+      <ClerkProvider
+        appearance={{
+          baseTheme: dark,
+        }}
+      >
+        {children}
+      </ClerkProvider>
+    );
+  } else {
+    return <ClerkProvider>{children}</ClerkProvider>;
+  }
 }
