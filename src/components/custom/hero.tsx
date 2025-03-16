a475ec7064ac0b739f5aac330a804a0fb9a07839
Micheal-Repo <michealfullstack@gmail.com>
2025-03-16 15:00:03 +0100

    hero

--- src/components/custom/hero.tsx
+++ src/components/custom/hero.tsx
@@ -3,11 +3,12 @@ import { Button } from "@/components";
 
 export default function Hero() {
   return (
-    <div className="w-screen min-h-[45vh] relative pt-8 ">
-      <div className="absolute inset-0 bg-gradient-to-b from-black/10 to-white dark:from-white/10 dark:to-black" />
+    <div className="w-screen h-[45vh] relative pt-8 ">
+      <div className="absolute inset-0 bg-gradient-to-b from-black/10 dark:from-white/15 to-background" />
 
       <div className="container relative mx-auto h-full flex flex-col padding-sp justify-center">
-        <div className="max-w-3xl space-y-4 md:space-y-6 ">
+        
+        <div className="max-w-md space-y-4 md:space-y-6 ">
           <h1 className="font-bold text-5xl md:text-6xl bg-gradient-to-r from-foreground to-foreground/80 bg-clip-text text-transparent">
             Expand Your Knowledge with Our Courses
           </h1>
@@ -17,7 +18,7 @@ export default function Hero() {
             level.
           </p>
 
-          <div className="mt-4">
+          <div className="mt-6">
             <Button size="lg" className="flex items-center gap-2 md:hidden">
               <BookMarkedIcon size={25} />
               <p className="">My Courses</p>
