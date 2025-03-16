29ebfd62a141f9c1a5cdd790c99ab1a35733714b
Micheal-Repo <michealfullstack@gmail.com>
2025-03-16 15:06:38 +0100

    hero

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -9,8 +9,12 @@ export default async function Home(){
 
   
   return (
-      <div className="w-screen min-h-screen bg-background  font-bold ">
+      <div className="w-screen min-h-screen bg-background ">
         <Hero/>
+        <div className="mx-auto container padding-sp">
+          <p className="text-muted-foreground font-semibold text-center">Featured Courses</p>
+          
+        </div>
       </div>
     )
 }
--- src/components/custom/hero.tsx
+++ src/components/custom/hero.tsx
@@ -3,10 +3,10 @@ import { Button } from "@/components";
 
 export default function Hero() {
   return (
-    <div className="w-screen relative pt-10 ">
+    <div className="w-screen relative pt-10 min-h-[45vh]">
       <div className="absolute inset-0 bg-gradient-to-b from-black/10 dark:from-white/15 to-background" />
 
-      <div className="container relative mx-auto h-full flex flex-col padding-sp justify-center">
+      <div className="container relative mx-auto padding-sp ">
         
         <div className="max-w-md space-y-4 md:space-y-6 ">
           <h1 className="font-bold text-5xl md:text-6xl bg-gradient-to-r from-foreground to-foreground/80 bg-clip-text text-transparent">
@@ -18,7 +18,7 @@ export default function Hero() {
             level.
           </p>
 
-          <div className="mt-6">
+          <div className="mt-10">
             <Button size="lg" className="flex items-center gap-2 md:hidden">
               <BookMarkedIcon size={25} />
               <p className="">My Courses</p>
