d5701517d4f5d27c2c11309dce53a28c02952458
Micheal-Repo <michealfullstack@gmail.com>
2025-03-17 12:36:32 +0100

    studio

--- other/sanity/env.ts
+++ other/sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-17'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-10'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
