c4d68d0b47a8223ef666360715c26a63ea5e8777
Micheal-Repo <michealfullstack@gmail.com>
2025-03-18 17:52:09 +0100

    course

--- src/components/custom/sidebar.tsx
+++ src/components/custom/sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
-import Link from "next/link"
+import Link from "next/link";
 
 //icons
 import {
@@ -22,6 +22,13 @@ import {
 } from "@/components/ui/tooltip";
 import { Button, ModeToggle } from "@/components";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Progress } from "@/components/ui/progress";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -42,14 +49,14 @@ export default function Sidebar() {
         <div
           className={cn(
             "absolute top-0 left-0 h-screen z-20 w-[calc(100vw-2.5rem)] sm:w-[calc(100vw-10rem)] border-r border-border bg-background border-r border-border transition-all duration-300",
-            isOpen ? "translate-x-[2.5rem]" : "-translate-x-[100%]"
+            isOpen ? "translate-x-[2.5rem]" : "-translate-x-[110%]"
           )}
         >
           <SidebarContent />
         </div>
 
         <div className="relative z-30 bg-background flex justify-center py-6 h-screen w-full border-r border-border">
-          <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-4">
             <TooltipProvider delayDuration={0}>
               <Tooltip>
                 <TooltipTrigger asChild>
@@ -95,6 +102,9 @@ export default function Sidebar() {
 }
 
 function SidebarContent() {
+  const [progress, setProgress] = useState(50);
+  const [activeModules, setActiveModules] = useState<string[]>([]);
+
   const course = {
     title: "Zero to fullstack Hero",
     category: "fullstack development",
@@ -132,15 +142,61 @@ function SidebarContent() {
       },
     ],
   };
-  
+
   return (
-    <ScrollArea className="w-full h-screen padding-sp ">
+    <ScrollArea className="w-full h-screen px-4 sm:px-6 ">
       <div className="w-full flex items-center justify-between py-4 mb-6">
-        <span>
-          <ArrowLeft size={24} /> My Courses
+        <span className="flex items-center gap-2">
+          <ArrowLeft size={24} /> 
+          <span>My Courses</span>
         </span>
         <ModeToggle />
       </div>
+
+      {/*title and progress*/}
+      <div>
+        <h2 className="text-3xl font-bold mb-4">{course.title}</h2>
+        <div>
+          <div className="w-full flex justify-center items-center">
+            <p className="text-sm text-muted-foreground">course progress</p>
+            <p className="text-sm text-muted-foreground">{progress}%</p>
+          </div>
+          <Progress value={progress} className="w-full" />
+        </div>
+      </div>
+
+      {/*modules*/}
+      <Accordion
+        type="multiple"
+        className="w-full space-y-4"
+        value={activeModules}
+        onValueChange={setActiveModules}
+      >
+        {course.modules.map((item, i) => (
+          <AccordionItem key={i} value={`item-${i}`}>
+            <AccordionTrigger className={`hover:no-underline py-3 px-3 ${i === 0 ? "border-t" : ""}`}>
+              <div className="flex items-center gap-4">
+                <span className="text-muted-foreground font-medium">0{i+1}</span>
+                <div className="flex flex-col gap-1">
+                  <p className="font-medium capitalize">{item.title}</p>
+                  <p className="text-xs text-muted-foreground">{item.lessons.length}{""} lessons</p>
+                </div>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent>
+              <div className="flex flex-col bg-muted/40">
+                {item.lessons.map((lesson,i)=>(
+                 <div key={i} className={`w-full py-3 pl-8 pr-3 hover:bg-muted/80 flex items-center gap-4 ${i === 0 ? "border-l-green-500 bg-muted/80" : ""}`}>
+                    <span className="text-muted-foreground">0{i+1}</span>
+                    <PlayCircle className="text-muted-foreground ml-2"/>
+                    <p className="capitalize">{lesson.title}</p>
+                 </div>
+                ))}
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </ScrollArea>
   );
 }
